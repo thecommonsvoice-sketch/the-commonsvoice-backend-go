10b07511e5e46c25be6a0433d91024e9af979f24
Priyanshu Negi <npriyanshu63@gmail.com>
2026-05-26 16:30:39 +0530

    Configure SQL connection pool to optimize query latency and prevent reconnect delays

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log/slog"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,6 +15,14 @@ func Connect(dsn string) (*gorm.DB, error) {
 
 	if err != nil {
 		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err == nil {
+		sqlDB.SetMaxIdleConns(10)
+		sqlDB.SetMaxOpenConns(50)
+		sqlDB.SetConnMaxLifetime(time.Hour)
+		sqlDB.SetConnMaxIdleTime(10 * time.Minute)
 	}
 
 	slog.Info("Database connected")
